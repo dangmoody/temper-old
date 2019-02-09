1c945c84402d5bcef7c6b61ab674b551cf28456f
Dan Moody <daniel.guy.moody@gmail.com>
2019-02-09 13:46:32 +0000

    demo test callbacks

--- example_suite.c
+++ example_suite.c
@@ -18,6 +18,18 @@ void OnSuiteEnded( void* data ) {
 	printf( "Test suite finished.\n" );
 }
 
+void OnTestStarting( void* data ) {
+	( (void) data );
+
+	printf( "Test starting.\n" );
+}
+
+void OnTestEnded( void* data ) {
+	( (void) data );
+
+	printf( "Test finished.\n" );
+}
+
 NE_TEST( XShouldEqual0 ) {
 	float x = 0.0f;
 	NE_TEST_EXPECT_TRUE( floateq( x, 0.0f ) );
@@ -59,8 +71,13 @@ int main( int argc, char** argv ) {
 
 	NE_TEST_INIT();
 
+	// comment these out for better/stock looking output
+	// this is just to demo the callbacks
 	NE_TEST_SET_SUITE_START_CALLBACK( OnSuiteStarting, NULL );
 	NE_TEST_SET_SUITE_END_CALLBACK( OnSuiteEnded, NULL );
+	
+	NE_TEST_SET_TEST_START_CALLBACK( OnTestStarting, NULL );
+	NE_TEST_SET_TEST_END_CALLBACK( OnTestEnded, NULL );
 
 	NE_TEST_RUN_SUITE( TheSuite );
 
