88458b24a3836552de007627ab22f039b6126760
Dan Moody <daniel.guy.moody@gmail.com>
2019-02-09 14:58:08 +0000

    mix up the tests a little bit

--- examples/example_basic.c
+++ examples/example_basic.c
@@ -4,21 +4,21 @@
 
 NE_TEST( XShouldEqualString0 ) {
 	const char* msg = "This is a string";
-	NE_TEST_EXPECT_TRUE( strcmp( msg, "This is a string" ) == 0 );
+	NE_TEST_EXPECT_TRUE( strcmp( msg, "THiS iS a STRinG" ) == 0 );
 
 	NE_TEST_PASS();
 }
 
 NE_TEST( XShouldEqualString1 ) {
 	const char* msg = "This is a string";
-	NE_TEST_EXPECT_TRUE( strcmp( msg, "THiS iS a STRinG" ) == 0 );
+	NE_TEST_EXPECT_TRUE( strcmp( msg, "hoolaba)" ) == 0 );
 
 	NE_TEST_PASS();
 }
 
 NE_TEST( XShouldEqualString2 ) {
 	const char* msg = "This is a string";
-	NE_TEST_EXPECT_TRUE( strcmp( msg, "hoolaba)" ) == 0 );
+	NE_TEST_EXPECT_TRUE( strcmp( msg, "This is a string" ) == 0 );
 
 	NE_TEST_PASS();
 }
@@ -30,8 +30,8 @@ int main( int argc, char** argv ) {
 	NE_TEST_INIT();
 
 	NE_TEST_RUN_TEST( XShouldEqualString0 );
-	NE_TEST_RUN_TEST( XShouldEqualString1 );
-	NE_TEST_SKIP_TEST( XShouldEqualString2, "Test skipping test not in a suite." );
+	NE_TEST_SKIP_TEST( XShouldEqualString1, "Test skipping test not in a suite." );
+	NE_TEST_RUN_TEST( XShouldEqualString2 );
 
 	NE_TEST_SHUTDOWN();
 
