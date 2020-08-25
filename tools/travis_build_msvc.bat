6d312c965e8a1114d35ef1fb81362d970ab7128c
dan <daniel.guy.moody@gmail.com>
2020-08-26 00:59:40 +0100

    fix travis win32/MSVC

--- tools/travis_build_msvc.bat
+++ tools/travis_build_msvc.bat
@@ -8,7 +8,7 @@ if [%cpp%]==[] (
 	goto :bail
 )
 
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
 
 pushd %~dp0
 
